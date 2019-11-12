a73b60877bab4c44e15f837bbce2537414f9a84a
ngaosenpai <longbestlee1909@gmail.com>
2019-11-12 14:59:34 +0700

    update intopo for negative number

--- core/infix_to_postfix.py
+++ core/infix_to_postfix.py
@@ -2,7 +2,9 @@ from supports import support_calculating as sC
 def infix_to_postfix(express):
 	Postfix = []
 	stack = []
-	for i in express:
+	infix = sC.make_infix(express)
+	print(infix)
+	for i in infix:
 		if not sC.isOperator(i) and i != "(" and i != ")":
 			Postfix.append(i)
 		else:
@@ -12,7 +14,7 @@ def infix_to_postfix(express):
 				if len(stack) > 0:
 					stack.pop()      
 			else:
-				while len(stack) > 0 and stack[-1] != "(" and i != "(" and sC.Priority(stack[-1]) < sC.Priority(i):
+				while len(stack) > 0 and stack[-1] != "(" and i != "(" and sC.Priority(stack[-1]) <= sC.Priority(i):
 					Postfix.append(stack.pop())
 				stack.append(i)
 
--- supports/support_calculating.py
+++ supports/support_calculating.py
@@ -1,4 +1,5 @@
 import re
+import math
 ####### priority function
 def Priority(data):
 	if isOperator(data):
@@ -13,8 +14,32 @@ def Priority(data):
 		if data == "-":
 			return 3
 	else:
-		return 0
+		return None
 
 ######## isOperator function
 def isOperator(data):
-	return re.search("[\^*/+-]", data)
+	if len(data) == 1:
+		return re.search("[\^*/+-]", data)
+	else: 
+		return None
+
+####### make_infix function
+def make_infix(express):
+	infix = []
+	temp = ""
+	for i in range(len(express)):
+		if express[i].isdigit():
+			temp += express[i]
+		elif express[i] == "-" and (i == 0 or i != 0 and (isOperator(express[i-1]) or express[i-1] == "(" )):
+				temp += "-"
+				continue
+		else:
+			if temp == "":
+				infix.append(express[i])
+			else:
+				infix.append(temp)
+				infix.append(express[i])
+				temp = ""
+	if temp.isdigit() or temp.lstrip("-").isdigit():
+		infix.append(temp)
+	return infix

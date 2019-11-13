fd9ad77e95bef98947b031315bf79c53977a0726
ngaosenpai <longbestlee1909@gmail.com>
2019-11-13 10:03:53 +0700

    debug optionA

--- core/cal_postfix_to_result.py
+++ core/cal_postfix_to_result.py
@@ -1,10 +1,11 @@
 import math
 def Cal(Postfix): 
     stack = []
+    print(Postfix)
     try:
         for i in Postfix:
             if (i.isdigit() or i.lstrip("-").isdigit()): 
-                stack.append(int(i))
+                 stack.append(int(i))
             else: 
                 if i == '^': 
                     stack_sencond = stack.pop()
@@ -22,6 +23,8 @@ def Cal(Postfix):
                 elif i == '-':
                     stack_sencond = stack.pop()
                     stack.append(stack.pop() - stack_sencond)
+                else:
+                    stack = ["Math Error !"]
     except:
         stack = ["Math Error !"]
     return stack[-1]

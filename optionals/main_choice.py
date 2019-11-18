49d865bfbd0c485b9d7fedb397d9f34036c888e1
shanenoi <shanenoi.org@gmail.com>
2019-11-18 19:25:32 +0700

    reorganize verify_input

--- optionals/main_choice.py
+++ optionals/main_choice.py
@@ -29,24 +29,6 @@ def choice():
         result = ptr.Cal(postfix)
         print("result = ", result)
     elif(response == "B"):
-
-        def correct_input(*args):
-            # check_option(len = 2): [option (A, B, C, D)], [send message if getting error]
-            if len(args) == 2:
-                response = vI.check(["A","B","C","D"], args[0])
-                while len(response) != 1:
-                    print(response)
-                    choose = input(args[1])
-                    response = vI.check(["A","B","C","D"], choose)
-                return response
-            # check_number(len = 3): [number], [send message if getting error], [option of (A, B, C, D)]
-            elif len(args) == 3:
-                number = args[0]
-                while not cR.check_number(number, args[2]):
-                    print("[!] Input Error")
-                    number = input(args[1])
-                return number
-
         template = lambda message: (
             f"\n--- {message} ---\n\n" +
             "A. Binary(2)\n" +
@@ -56,19 +38,19 @@ def choice():
             "[Type]╼> "
         )
 
-        input_option = correct_input(
+        input_option = vI.check_radix_converting(
             input(template("Type of the input number")),
             "[Type]╼> ",
         ).upper()
 
-        number = correct_input(
+        number = vI.check_radix_converting(
             input(
                 f"\n[{cR.option_type[input_option][0]}]╼> "
             ),
             f"\n[{cR.option_type[input_option][0]}]╼> ",
             input_option
         )
-        output_option = correct_input(
+        output_option = vI.check_radix_converting(
             input(template("Type of the number you want to convert")),
             "[Type]╼> "
         ).upper()
--- supports/verify_input.py
+++ supports/verify_input.py
@@ -1,13 +1,34 @@
+from core import convert_radix as cR
 import re
+
+
 def check(keys, input):
-	if len(input) > 1:
-		return "plz! Just type 1 character"
-	else:
-		strKeys = "["
-		for key in keys:
-			strKeys += key + ","
-		strKeys += "]"
-		if re.search(strKeys, input.upper()):
-			return input.upper()
-		else:
-			return "Wrong key!"
+    if len(input) > 1:
+        return "plz! Just type 1 character"
+    else:
+        strKeys = "["
+        for key in keys:
+            strKeys += key + ","
+        strKeys += "]"
+        if re.search(strKeys, input.upper()):
+            return input.upper()
+        else:
+            return "Wrong key!"
+
+
+def check_radix_converting(*args):
+    # check_option(len = 2): [option (A, B, C, D)], [send message if getting error]
+    if len(args) == 2:
+        response = check(["A", "B", "C", "D"], args[0])
+        while len(response) != 1:
+            print(response)
+            choose = input(args[1])
+            response = check(["A", "B", "C", "D"], choose)
+        return response
+    # check_number(len = 3): [number], [send message if getting error], [option of (A, B, C, D)]
+    elif len(args) == 3:
+        number = args[0]
+        while not cR.check_number(number, args[2]):
+            print("[!] Input Error")
+            number = input(args[1])
+        return number
